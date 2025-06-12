30d2ba7501be1be0ef5d86122a6e0bb5f24119c0
Shivam <kumarshivam916224@gmail.com>
2025-06-12 19:42:57 +0530

    Setup Completed

---
+++ client/src/main.jsx
@@ -0,0 +1,10 @@
+import './index.css'
+import React from 'react' 
+import ReactDOM from 'react-dom/client'
+
+
+function App() {
+  return <div className="text-2xl text-center mt-10 text-blue-500">Welcome To My MERN Portfolio!</div>
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
